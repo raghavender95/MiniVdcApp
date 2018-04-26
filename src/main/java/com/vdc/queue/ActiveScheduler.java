8fa5503ae98cf703e5135df9284cf395b6ab4cdd
vf-root <viraj.janardaningale@vodafone.com>
2018-04-26 14:48:23 +0530

    Viraj [26-04-2018]- Property File Changes

--- src/main/java/com/vdc/queue/ActiveScheduler.java
+++ src/main/java/com/vdc/queue/ActiveScheduler.java
@@ -8,27 +8,32 @@ import javax.jms.TextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@PropertySource("classpath:application.properties")
 @Component
 @EnableScheduling
 public class ActiveScheduler {
 
+	@Autowired 
+	Environment environment;
 
 	private static final Logger log = LoggerFactory.getLogger(ActiveScheduler.class);
 
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
+	
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
 	@Scheduled(fixedRate = 10000)
 	public void consumeMessage() {
 		try {
-			jmsTemplate.setDefaultDestinationName("ProcessingRequest");
+			jmsTemplate.setDefaultDestinationName(environment.getProperty("vdc.queue.ProcessingRequest.name"));
 
 			TextMessage textMessage = null;
 
@@ -39,7 +44,7 @@ public class ActiveScheduler {
 			String data = textMessage.getText();
 			if (data != null) {
 
-				jmsTemplate.setDefaultDestinationName("Delivery");
+				jmsTemplate.setDefaultDestinationName(environment.getProperty("vdc.queue.DeliveryRequestQueue.name"));
 				jmsTemplate.convertAndSend(textMessage);
 			}
 
